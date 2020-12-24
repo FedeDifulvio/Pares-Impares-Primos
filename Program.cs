6df51ba8dfb82fff8890514047f5fb91fcd480a7
FedeDifulvio <71615110+FedeDifulvio@users.noreply.github.com>
2020-12-24 17:11:31 -0300

    MostrarPares, MostrarImpares, MostrarPrimos

--- Program.cs
+++ Program.cs
@@ -22,21 +22,87 @@ namespace ListasPredicados
                 }   
                 catch (FormatException)
                 {
-                    Console.WriteLine("ERROR: debe ingresar un número");
+                    Console.WriteLine("ERROR: debe ingresar un número entero");
                    
                 }
                 
             }
-            ListaNumeros.RemoveAt(ListaNumeros.Count - 1 ); 
+            ListaNumeros.Remove(0);  
+            Console.Clear();
 
-            Console.WriteLine("Fin INGRESO");  
+            Console.WriteLine("LISTADO DE NÚMEROS PARES: ");
+            MostrarPares(ListaNumeros);
+            Console.WriteLine("LISTADO DE NÚMEROS IMPARES: ");
+            MostrarImpares(ListaNumeros); 
+            Console.WriteLine("LISTADO DE NÚMEROS PRIMOS: " );
+            MostrarPrimos(ListaNumeros);
+           
 
-            foreach( int number in ListaNumeros)
+
+
+        }
+
+        static bool EsPar(int num)
+        {
+            if (num % 2 == 0) return true;
+            else return false; 
+        }
+
+        static bool EsImpar(int num)
+        {
+            if (num % 2 == 0) return false;
+            else return true;
+        }
+
+        static bool EsPrimo(int num)
+        {
+            int contador = 0; 
+            for( int x=1; x<=num; x++)
+            {
+                if (num % x == 0)
+                {
+                    contador++; 
+                }
+            }
+            if (contador == 2) return true;
+            else return false; 
+            
+        } 
+
+        static void MostrarPrimos(List<int> numeros)
+        {
+            Predicate<int> buscador = new Predicate<int>(EsPrimo);
+
+            List<int> NumerosPrimos = numeros.FindAll(buscador);
+
+            foreach (int number in NumerosPrimos)
             {
                 Console.WriteLine(number); 
             }
+        } 
 
+        static void MostrarPares(List<int> numeros)
+        {
+            Predicate<int> buscador = new Predicate<int>(EsPar);
 
+            List<int> NumerosPares = numeros.FindAll(buscador);
+
+            foreach (int number in NumerosPares)
+            {
+                Console.WriteLine(number);
+            }
+
+        }
+
+        static void MostrarImpares(List<int> numeros)
+        {
+            Predicate<int> buscador = new Predicate<int>(EsImpar);
+            List<int> NumerosImpares = numeros.FindAll(buscador);
+
+            foreach (int number in NumerosImpares)
+            {
+                Console.WriteLine(number);
+            }
         }
     }
 }
