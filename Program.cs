b68ec7ee6964597fdc2089bc2ea46d2caaea2603
FedeDifulvio <71615110+FedeDifulvio@users.noreply.github.com>
2020-12-24 16:07:58 -0300

    primer commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListasPredicados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> ListaNumeros = new List<int>();
+            int numero = 1; 
+
+            Console.WriteLine("Ingrese los números que desee y le informaremos cuales son PARES, IMPARES y PRIMOS. ");
+            Console.WriteLine("Cuando quiera finalizar el ingreso de datos, presione cero");  
+
+            while(numero != 0)
+            {
+                try
+                {
+                    numero = int.Parse(Console.ReadLine());
+                    ListaNumeros.Add(numero);
+                }   
+                catch (FormatException)
+                {
+                    Console.WriteLine("ERROR: debe ingresar un número");
+                   
+                }
+                
+            }
+            ListaNumeros.RemoveAt(ListaNumeros.Count - 1 ); 
+
+            Console.WriteLine("Fin INGRESO");  
+
+            foreach( int number in ListaNumeros)
+            {
+                Console.WriteLine(number); 
+            }
+
+
+        }
+    }
+}
